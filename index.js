7b32e62184b090215a024054f62c7e899243cf25
Micah Banschick <micha.ban@gmail.com>
2020-03-18 17:51:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+
+let element = document.createElement('h1')
+
+element.id = 'victory'
+
+document.body.appendChild(element)
+
+const newHeader = document.querySelector('h1#victory')
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
