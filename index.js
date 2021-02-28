88b108afc99a225bbb737f6ee16d793f0f8acacf
Micah Banschick <micha.ban@gmail.com>
2021-02-28 19:30:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
 document.querySelector('main#main').remove()
+<<<<<<< HEAD
 
 let element = document.createElement('h1')
 
@@ -10,3 +11,7 @@ document.body.appendChild(element)
 const newHeader = document.querySelector('h1#victory')
 
 newHeader.innerHTML = "YOUR-NAME is the champion"
+=======
+const newHeader = document.querySelector('h1#victory')
+newHeader.innerHTML = 
+>>>>>>> 895b756478743e9b9bed0e5ee9ab10486b2049cb
